c31112e05efe7d3c92339e8a23488c47d7e8b2d9
Marjorie Lapointe <marjorie.lapointe@zaclys.net>
2025-06-11 10:49:09 +0200

    threads

--- solutions/20_threads/threads1.rs
+++ solutions/20_threads/threads1.rs
@@ -22,7 +22,7 @@ fn main() {
 
     let mut results = Vec::new();
     for handle in handles {
-        // TODO: Collect the results of all threads into the `results` vector.
+        // Collect the results of all threads into the `results` vector.
         results.push(handle.join().unwrap());
     }
 
