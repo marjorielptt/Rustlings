d1feaf7bcb41abd8ee18238e238bf58df88455b5
Marjorie Lapointe <marjorie.lapointe@zaclys.net>
2025-06-11 13:40:05 +0200

    clippy

--- solutions/22_clippy/clippy2.rs
+++ solutions/22_clippy/clippy2.rs
@@ -1,7 +1,7 @@
 fn main() {
     let mut res = 42;
     let option = Some(12);
-    // TODO: Fix the Clippy lint.
+    // Use `if-let` instead of iteration.
     if let Some(x) = option {
         res += x;
     }
